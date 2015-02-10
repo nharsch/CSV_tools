324d7291babfda961e62bf4ee25190f295f83703
Bethany <bethany@bethany-MacBook.(none)>
2015-02-10 12:28:47 -0500

    added beginning update script

---
+++ CSV_update.py
@@ -0,0 +1,8 @@
+#  takes two like CSV files, updates the second
+
+def update(csv1, csv2, keyheader):
+    '''
+    update will return new csv with updated values from csv2
+    csv1 is reference update sheet
+    '''
+    
