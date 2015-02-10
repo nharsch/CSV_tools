90bca3b3ef2230d850ae9716d1eea69d970f076e
Bethany <bethany@bethany-MacBook.(none)>
2015-02-10 13:16:36 -0500

    added update script, still working

--- CSV_update.py
+++ CSV_update.py
@@ -1,8 +1,27 @@
 #  takes two like CSV files, updates the second
+from .CSV_tools import CsvTable 
 
-def update(csv1, csv2, keyheader):
-    '''
-    update will return new csv with updated values from csv2
-    csv1 is reference update sheet
-    '''
-    
+def CSV_update(CsvTable1, CsvTable2):
+	'''
+	csv compare loop
+	returns 
+	assumes both lsiters are lists of dicts
+	'''
+	for row2 in CsvTable2.dicter:
+		in_1 = False
+		for row1 in CsvTable1.dicter:
+			if row2['PRODID'] == row1['PRODID']:
+				row1.update(row2)
+				#row1['updated_last'] = now
+				in_1 = True
+		if not in_1:
+			CsvTable1.dicter.append(row2)
+	# return lister_DB
+
+def find_deletes(DB_lister):
+	'''
+	find any items on OVSDB but not on update
+	'''
+	for i, row in enumerate(DB_lister):
+		if row['last_updated'] != now:
+			del DB_lister[i]
